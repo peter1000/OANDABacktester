34b4f9fb397493a1f78523ac38aa139095dd6f01
Kevin <Kevincarlton777@gmail.com>
2013-12-10 17:22:23 -0500

    hey

--- TimeLord.py
+++ TimeLord.py
@@ -1,20 +1,58 @@
 from fxObjects import Rate
+from fxObjects import MarketSnapshot
+import json
+
 class TimeLord(object):
 
 	def __init__(self):
 		self.name = 0
-		self.rate1 = Rate.Rate("EUR/USD", "time", "1", "2");
+		self.rate1 = Rate.Rate("EUR/USD", "time", "1", "2")
+		self.snapShots = []
+
+	def API_getRate(self, instrumentName):
+		#return self.snapShots[0].instruments['EUR/USD']
+		return 1.00
+
+	def API_buy(self, instrumentName, units):
+		return 1.00
+
+	def API_sell(self, instrument, units):
+		return 1.00
 
 
-	def initialize(self):
-		rate1 = Rate.Rate("EUR/USD", "time", "1", "2");
-		# read in tick data into object.
+	def readJSON(self):	
+
+		print "Loading JSON" 
+		json_data = json.load(open('json_data.json'))
+
+		instrumentName = json_data['instrument']
+		candles = json_data['candles']
+
+		for rate in candles:
+			# create snapshot
+			snap = MarketSnapshot.MarketSnapshot(rate["time"])
+			#create new rate
+			tmpRate = Rate.Rate(instrumentName, rate["time"], rate["openBid"], rate["openAsk"])
+			# add rate/instrument to snapshot
+			snap.addInstrument(instrumentName, tmpRate)
+			# add snapshot to snaplist list
+			self.snapShots.append(snap)
+
+#		for s in self.snapShots :
+#			print s.date
+#			print s.instruments
+
+	print "Json Loaded" 
 
 	def mainLoop(self):
-		print self.rate1.instrument
-		print self.rate1.time
-		print self.rate1.bid
-		print self.rate1.ask
+		print "Entering Main Loop"
+
+		for i in range(10):
+			print self.API_getRate("EUR/USD")
+
+	def initialize(self):
+		self.readJSON()
+
 
 def main():
 	 x = TimeLord()
--- fxObjects/MarketSnapshot.py
+++ fxObjects/MarketSnapshot.py
@@ -2,10 +2,13 @@ from Rate import *
 
 class MarketSnapshot(object):
 
-	def __init__(self):
-		self.rate1 = Rate.Rate("EUR/USD", "time", "1", "2");
-		self.instruemnts = dict("EUR/USD" = self.rate1)
+	def __init__(self, date):
+		self.instruments = {}
+		self.date = date
 
 
-	
-		
+	def addInstrument(self, name, rate):
+		self.instruments[name]=rate
+
+	def getRate(instrument):
+		return self.instruments[instrument]
